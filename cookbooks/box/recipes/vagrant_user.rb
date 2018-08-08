d110568f13cf6f6cfe61e5f6d9a457ec8672a47c
Robert Ferney <rob@ferney.org>
2018-08-07 23:23:19 -0600

    updating cookbooks

--- cookbooks/box/recipes/vagrant_user.rb
+++ cookbooks/box/recipes/vagrant_user.rb
@@ -1,6 +1,5 @@
 
 user 'vagrant' do
-  password 'vagrant'
 end
 
 group 'vagrant' do
@@ -24,4 +23,62 @@ directory "/home/vagrant/.ssh" do
   mode '0700'
 end
 
+file "/etc/sudoers.d/99_vagrant" do
+  content "vagrant ALL=(ALL) NOPASSWD:ALL"
+  mode '0440'
+end
+
+directory "/etc/lightdm/lightdm.conf.d" do
+  recursive true
+end
+
+file "/etc/lightdm/lightdm.conf.d/50-myconfig.conf" do
+  content <<~CONF
+    [SeatDefaults]
+    autologin-user=vagrant
+    AUTOLOGIN
+  CONF
+end
+
+directory "/home/vagrant/.config/terminator" do
+  recursive true
+  user 'vagrant'
+  group 'vagrant'
+  mode '0700'
+end
+
+cookbook_file "/home/vagrant/.config/terminator/config" do
+  source 'terminator_config'
+  user 'vagrant'
+  group 'vagrant'
+  mode '0700'
+end
+
+directory "/home/vagrant/.config/xfce4/terminal" do
+  recursive true
+  user 'vagrant'
+  group 'vagrant'
+  mode '0700'
+end
+
+cookbook_file "/home/vagrant/.config/xfce4/terminal/terminalrc" do
+  source 'xfce_terminalrc'
+  user 'vagrant'
+  group 'vagrant'
+  mode '0700'
+end
+
+directory "/home/vagrant/.config/xfce4/panel" do
+  recursive true
+  user 'vagrant'
+  group 'vagrant'
+  mode '0700'
+end
+
+cookbook_file "/home/vagrant/.config/xfce4/panel/whiskermenu-1.rc" do
+  source 'whiskermenu.rc'
+  user 'vagrant'
+  group 'vagrant'
+  mode '0700'
+end
 
